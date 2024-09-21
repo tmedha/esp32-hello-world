50b6b565a042086a8c14f4a3f3715780f73c8947
Medha <tmedha01@gmail.com>
2024-09-21 03:41:12 -0500

    Turn blue light on

---
+++ on_blue_light.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello World, Medha here.");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
